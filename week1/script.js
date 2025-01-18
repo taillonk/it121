7c5b1c18bd3d8d8c5128b611ea24654ffeaca331
Kevin Sanchez Hernandez <114366872+taillonk@users.noreply.github.com>
2025-01-18 03:20:12 +0000

    HW 1 - correction

--- week1/script.js
+++ week1/script.js
@@ -2,5 +2,3 @@ const firstName = "Kevin";
 const lastName = "Sanchez Hernandez";
 let yearsOfStudy = 1;
 let goal = "change my career from Payroll Analyst to Software Engineer";
-
-document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " year, and plan to " + goal + " after completing my courses.");
